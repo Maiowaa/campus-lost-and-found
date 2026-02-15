450e7d0d58b6218e13b89cf1a3b8c7bcfe722ed2
Kushagar Sood <soodkushagar387@gmail.com>
2026-02-15 20:59:45 +0530

    Add auto superuser creation in build script

--- build.sh
+++ build.sh
@@ -6,3 +6,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Auto-create superuser if env vars are set
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ]; then
+    python manage.py createsuperuser --no-input || true
+fi
